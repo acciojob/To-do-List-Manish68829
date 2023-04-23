2393d033b1811df7d1e314a8ab71c837a16281f2
Manish <121147194+Manish68829@users.noreply.github.com>
2023-04-23 18:08:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var addToList=document.getElementById("list");
 
 addBtn.addEventListener("click",()=>{
 	var taskValue=task.value.trim();
-	if(taskValue!=""){
+	if(taskValue!==""){
 		const todoItem=document.createElement("li");
 		todoItem.innerText=taskValue;
 		list.appendChild(todoItem);
